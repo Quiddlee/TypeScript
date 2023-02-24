9707654f125d225bfdac988115356032ec59f783
Bogdan <thegarrybit@gmail.com>
2023-02-24 13:31:14 +0200

    never & unknown types

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,4 +1,15 @@
 let userInput: unknown;
+let userName: string;
 
 userInput = 5;
 userInput = 'Bogdan';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+    throw { message: message, errorCode: code };
+    // while(true) {}
+} 
+
+generateError('An error occurred!', 500);
