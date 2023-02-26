df956fdf90986743ac0a550b06b76279f5856ead
Bogdan <thegarrybit@gmail.com>
2023-02-26 16:40:07 +0200

    decorators

--- notes/decorators/src/app.ts
+++ notes/decorators/src/app.ts
@@ -8,13 +8,17 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
     console.log('TEMPLATE FACTORY');
-    return function(constructor: any) {
-        console.log('Rendering template');
-        const hookEl = document.querySelector(`#${hookId}`);
-        const person = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1')!.textContent = person.name;
+    return function<T extends { new(...args: any[]): { name: string } }>(originalConstructor: T) {
+        return class extends originalConstructor {
+            constructor(..._: any[]) {
+                super();
+                console.log('Rendering template');
+                const hookEl = document.querySelector(`#${hookId}`);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1')!.textContent = this.name;
+                }
+            }
         }
     }
 }
@@ -33,3 +37,85 @@ class Person {
 // const pers = new Person();
 
 // console.log(pers);
+
+// ---
+
+function Log(target: any, propertyName: string | symbol) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accessors decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log3(target: any, name: string | symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor); 
+}
+
+function Log4(target: any, name: string | symbol, position: number) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position); 
+}
+
+class Product {
+    @Log
+    title: string;
+
+    constructor(title: string, private _price: number) {
+        this.title = title;
+    }
+
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
+        return this._price * (1 + tax);
+    }
+    
+    @Log2
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('Invalid price - should be positive!');
+        }
+    }
+}
+
+const p1 = new Product('Book', 19);
+const p2 = new Product('Book 2', 29);
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };  
+    return adjustedDescriptor;
+}
+
+class Printer {
+    message = 'This works!';
+
+    @Autobind
+    showMessage() {
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button') as HTMLButtonElement;
+button.addEventListener('click', p.showMessage);
